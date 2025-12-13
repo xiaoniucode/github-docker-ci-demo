00333974eb203a47187e6db44b69f1c575aafd9b
xiaoniucode <yuanyicode@gmail.com>
2025-12-14 00:15:10 +0800

    add code

---
+++ src/main/java/com/xiaoniucode/gdcd/GdcdApplication.java
@@ -0,0 +1,15 @@
+package com.xiaoniucode.gdcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuxin
+ */
+@SpringBootApplication
+public class GdcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdcdApplication.class, args);
+    }
+}
